757ffa537773a9d6fea1ffeb4fe8c9f5a1ddc23b
AbhiAtiUttam <Abhi@glocalize.net>
2024-08-30 08:55:20 +0530

    added models

--- src/components/cube/Cube.jsx
+++ src/components/cube/Cube.jsx
@@ -2,23 +2,35 @@ import { OrbitControls } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber';
 import React, { useRef } from 'react'
 
-function Cube() {
+// import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+function Cube(props) {
+  
+  const { nodes, materials } = useGLTF('/model/koreanwinggltf.gltf')
+    // const cubeRef = useRef()
 
-    const cubeRef = useRef()
-
-    useFrame((state,delta)=>{
-        cubeRef.current.rotation.y +=0.005
-        });
+    // useFrame((state,delta)=>{
+    //     cubeRef.current.rotation.y +=0.005
+    //     });
   return (
     <>
     <OrbitControls/>
     <ambientLight/>
-    <mesh ref={cubeRef}>
+    {/* <mesh ref={cubeRef}>
         <boxGeometry />
         <meshStandardMaterial color={"mediumpurple"}/>
-    </mesh>
+    </mesh> */}
+  <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_0007.geometry}
+        material={materials['Material_0.008']}
+      />
+    </group>
     </>
   )
 }
 
+useGLTF.preload('/model/koreanwinggltf.gltf')
 export default Cube
